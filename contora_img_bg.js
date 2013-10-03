6173985bc47199df80aca685b83ba8e2815f56c6
Dima Evseev <d.melnick@contorra.ru>
2013-10-03 13:08:46 +0400

    v3

--- contora_img_bg.js
+++ contora_img_bg.js
@@ -4,26 +4,25 @@
         $('.bg-preload').not('.init').each(function(key, element) {
             $(element).addClass('init');
             $(element).after('<div class="preloader"><div class="preloader_container"></div></div>');
-
             console.log('Начинаем загрузку.');
+            var url = $(element).data('url');
+            var id = $(element).data('id');
+            var sel = '*[data-id=' + id + ']';
 
-            $.imgpreload($(element).data('url'), function() {
-
-                //
-                // Магия анимированного появления картинки пишется здесь.
-                //
-
-                $(element).css('background-image', 'url(' + $(element).data('url') + ')');
-                $(element).addClass('loaded');
-                $(element).next('.preloader').addClass('loaded');
+            $.imgpreload(url, function() {
+                $(sel).css('background-image', 'url(' + url + ')');
+                $(sel ).addClass('loaded');
+                $(sel).next('.preloader').addClass('loaded');
             });
         });
     }
 
-    Drupal.behaviors.contora_img_bg = {
-        attach: function (context, settings) {
-            bg_preloader_init ();
-        }
-    };
+    $(document).ready(function(){
+        bg_preloader_init ();
+    });
+
+    $(document).ajaxComplete(function(){
+        bg_preloader_init ();
+    });
 
 })(jQuery);
